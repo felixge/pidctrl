121a31a0118d99ee4d4e0ebf35a6b9da9cad7b20
Felix Geisendörfer <felix@debuggable.com>
2013-11-03 11:18:22 +0100

    README, LICENSE, and Update()

---
+++ README.md
@@ -0,0 +1,15 @@
+# pidctrl
+
+A [PIDController][1] implementation in [Go][2].
+
+## API Documentation
+
+http://godoc.org/github.com/felixge/pidctrl
+
+
+## License
+
+MIT License, see LICENSE.txt file.
+
+[1]: http://en.wikipedia.org/wiki/PID_controller
+[2]: http://golang.org/
--- pidtrl.go
+++ pidtrl.go
@@ -1,32 +1,53 @@
+// Packege pidctrl implements a PID controller.
+//
+// see http://en.wikipedia.org/wiki/PID_controller
 package pidctrl
 
 import (
 	"time"
 )
 
+// NewPIDController returns a new PIDController using the given gain values.
 func NewPIDController(p, i, d float64) *PIDController {
 	return &PIDController{p: p, i: i, d: d}
 }
 
+// PIDController implements a PID controller.
 type PIDController struct {
-	p          float64
-	i          float64
-	d          float64
-	setpoint   float64
-	prevErr    float64
-	integral   float64
-	lastUpdate time.Time
+	p          float64   // proportional gain
+	i          float64   // integral gain
+	d          float64   // derrivate gain
+	setpoint   float64   // current setpoint
+	prevErr    float64   // error from last update
+	integral   float64   // integral sum
+	lastUpdate time.Time // time of last update
 }
 
+// Set changes the setpoint of the controller.
 func (c *PIDController) Set(setpoint float64) {
 	c.setpoint = setpoint
 }
 
+// Update is identical to UpdateDuration, but automatically keeps track of the
+// durations between updates.
+func (c *PIDController) Update(value float64) float64 {
+	var duration time.Duration
+	if !c.lastUpdate.IsZero() {
+		duration = time.Since(c.lastUpdate)
+	}
+	c.lastUpdate = time.Now()
+	return c.UpdateDuration(value, duration)
+}
+
+// UpdateDuration updates the controller with the given value and duration since
+// the last update. It returns the new output.
+//
+// see http://en.wikipedia.org/wiki/PID_controller#Pseudocode
 func (c *PIDController) UpdateDuration(value float64, duration time.Duration) float64 {
 	var (
-		dt = duration.Seconds()
+		dt  = duration.Seconds()
 		err = c.setpoint - value
-		d float64
+		d   float64
 	)
 	c.integral += err * dt
 	if dt > 0 {
